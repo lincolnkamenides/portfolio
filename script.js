f15d1102097bfb84002f2fee7adfad8bea8d0288
lincolnkamenides <46967860+lincolnkamenides@users.noreply.github.com>
2020-11-30 23:38:20 -0600

    added menu overlay

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function myFunction(x) {
   x.classList.toggle("change");
+
+var y = document.getElementById("menu");
+  if (y.style.display === "block") {
+    y.style.display = "none";
+  } else {
+    y.style.display = "block";
+  }
 }
