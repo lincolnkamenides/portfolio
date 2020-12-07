a03133b4cb05e7c68ddd6783180270f28c6635f3
lincolnkamenides <46967860+lincolnkamenides@users.noreply.github.com>
2020-12-06 22:09:30 -0600

    fixed sidebar/added picture modal

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-function myFunction(x) {
-  x.classList.toggle("change");
+function openSideBar() {
+  var list = document.getElementById("menu-button");
+  list.classList.toggle("change");
 
   var y = document.getElementById("menu");
   if (y.style.display === "block") {
@@ -7,4 +8,27 @@ function myFunction(x) {
   } else {
     y.style.display = "block";
   }
+}
+
+function openImage(x) {
+ // Get the modal
+ var modal = document.getElementById('myModal');
+
+ // Get the image and insert it inside the modal - use its "alt" text as a caption
+ var img = x;
+ var modalImg = document.getElementById("img01");
+ var captionText = document.getElementById("caption");
+ img.onclick = function(){
+   modal.style.display = "block";
+   modalImg.src = this.src;
+   captionText.innerHTML = this.alt;
+ }
+
+ // Get the <span> element that closes the modal
+ var span = document.getElementsByClassName("close")[0];
+
+ // When the user clicks on <span> (x), close the modal
+ span.onclick = function() { 
+   modal.style.display = "none";
+ }
 }
