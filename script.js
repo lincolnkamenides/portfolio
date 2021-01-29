6aad585149d237d001205f550ef8e64469500655
lincolnkamenides <46967860+lincolnkamenides@users.noreply.github.com>
2021-01-28 19:26:03 -0600

    added first slideshow element

--- script.js
+++ script.js
@@ -32,3 +32,33 @@ function openImage(x) { //opens image in about with modal
    modal.style.display = "none";
  }
 }
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("thumbnail");
+  var slideCaptionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  slideCaptionText.innerHTML = dots[slideIndex-1].alt;
+}
+
