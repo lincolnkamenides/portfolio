2bf8abb20c8d68e2b0ffa5f729f66ed48deedfae
lincolnkamenides <46967860+lincolnkamenides@users.noreply.github.com>
2021-01-29 13:14:45 -0600

    fixed the close button

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ function openSlideShow(img, modal, slides) { //opens image in about with modal
  }
 
  // Get the <span> element that closes the modal
- var span = document.getElementsByClassName("close")[0];
+ var span = document.getElementsByClassName("close " + slides)[0];
 
  // When the user clicks on <span> (x), close the modal
  span.onclick = function() { 
